8be169dce3cc0f8da758e6989f8c60e87f51fde4
amiehusseinSSoori <amirhusseinsoori75@gmail.com>
2021-06-16 22:28:12 -0700

    add file proto

--- app/src/main/java/com/amirhusseinsoori/grpcmviarch/MainActivity.kt
+++ app/src/main/java/com/amirhusseinsoori/grpcmviarch/MainActivity.kt
@@ -2,10 +2,13 @@ package com.amirhusseinsoori.grpcmviarch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.grpc.domain.mizannodes.TurnOnRequest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ val kotlinVersion = "1.4.32"
 
 
 dependencies {
-    implementation("com.android.tools.build:gradle:4.1.3")
+    implementation("com.android.tools.build:gradle:4.2.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
     implementation("com.google.protobuf:protobuf-gradle-plugin:0.8.15")
